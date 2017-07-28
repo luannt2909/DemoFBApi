7aff611e9879bf547896501d5dd4590b711e4ad5
luannt2909 <luannt2909@gmail.com>
2017-07-28 08:43:29 +0700

    add service

---
+++ app/src/main/java/com/luannt/lap10515/demosimpleapp/service/MainService.java
@@ -0,0 +1,10 @@
+package com.luannt.lap10515.demosimpleapp.service;
+
+import android.app.Service;
+
+/**
+ * Created by lap10515 on 27/07/2017.
+ */
+
+public class MainService extends Service {
+}
